cf777e55eb35f4c2a5b87e2226c1a0100c27036d
Avneet Kaur Bhatia <avnibhatia15@gmail.com>
2022-09-18 02:24:30 +0530

    passing params

---
+++ 3-setTimeout-with-function-and-argument.js
@@ -0,0 +1,6 @@
+const fun1 = param => {
+  console.log(param + ' rocks');
+}
+
+setTimeout(fun1, 2000, 'Node');
+//3rd parameter is the input parameter.It will show in the param part of the function
