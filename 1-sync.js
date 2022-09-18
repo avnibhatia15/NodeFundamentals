7c26b15ef999450163692d5792629e3abfbedf17
Avneet Kaur Bhatia <avnibhatia15@gmail.com>
2022-09-18 17:02:21 +0530

    sync operation

--- 1-sync.js
+++ 1-sync.js
@@ -5,3 +5,4 @@ const data = fs.readFileSync(__filename);
 console.log('File data is', data);
 
 console.log('TEST');
+//the TEST is printed after the data because it is sync operation and in sync operation code is executed line by line
